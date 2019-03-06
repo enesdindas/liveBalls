120daa73492dd389e92ac85a009f2d88511c3c27
ns <enesdindas26@gmail.com>
2019-03-06 05:47:33 +0300

    newUser event

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,13 +1,40 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
-    const socket = io.connect('http://localhost:3000');
-    const connectionOptions = {
-        reconnectionAttemps: 3,
-        reconnection: 600
-    }
-    indexFactory.connectSocket('http://localhost:3000', connectionOptions, {
-    }).then((socket) => {
-       console.log('bağlantı gerçekleşti', socket) 
-    }).catch((err) => {
-        console.log(err)
-    });
+
+    $scope.messages = [];
+
+    $scope.init = () => {
+        const username = prompt('Please enter username');
+
+        if (username)
+            initSocket(username);
+        else
+            return false;
+    };
+
+    function initSocket(username) {
+        const connectionOptions = {
+            reconnectionAttemps: 3,
+            reconnection: 600
+        };
+
+        indexFactory.connectSocket('http://localhost:3000', connectionOptions)
+            .then((socket) => {
+                socket.emit('newUser', {username});
+
+                socket.on('newUser', (data)  => {
+                    console.log(data);
+                    const messageData = {
+                        type: 0, // info
+                        username: data.username
+                    };
+
+                    $scope.messages.push(messageData);
+                    $scope.$apply();
+                });
+            }).catch((err) => {
+                console.log(err)
+            });
+    };
+
+
 }]);
